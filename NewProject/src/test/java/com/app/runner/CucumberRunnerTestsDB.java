0c91a23a6b44b6ee679d179872990caf0922171d
hamid_raza <hamid_raza@EPINHYDW03C3>
2024-03-25 21:52:59 +0530

    Some practice

--- NewProject/src/test/java/com/app/runner/CucumberRunnerTestsDB.java
+++ NewProject/src/test/java/com/app/runner/CucumberRunnerTestsDB.java
@@ -1,5 +1,7 @@
 package com.app.runner;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -7,5 +9,11 @@ import io.cucumber.testng.CucumberOptions;
 		"com.app.Hooks" }, plugin = { "pretty", "html:target/cucumber-reports.html",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
 public class CucumberRunnerTestsDB extends AbstractTestNGCucumberTests {
+
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
 
 }
--- NewProject/src/test/java/com/app/runner/CucumberRunnerTestsUI.java
+++ NewProject/src/test/java/com/app/runner/CucumberRunnerTestsUI.java
@@ -1,11 +1,18 @@
 package com.app.runner;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "@UI", features = { "src/test/resources/features" }, glue = { "com.app.stepDefs",
 		"com.app.Hooks" }, plugin = { "pretty", "html:target/cucumber-reports.html",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = false)
 public class CucumberRunnerTestsUI extends AbstractTestNGCucumberTests {
 
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
 }
