9e45847e04ac7c21e567d6aaf846b2e79a6750a2
Hamid Raza <hamid_raza@epam.com>
2024-04-02 18:02:46 +0530

    Some cleanings

--- NewProject/src/test/java/com/app/stepDefs/dbOpsStepDefAPI.java
+++ NewProject/src/test/java/com/app/stepDefs/dbOpsStepDefAPI.java
@@ -12,18 +12,21 @@ import io.cucumber.java.en.Then;
 public class dbOpsStepDefAPI {
 
 	public final static int TIMEOUT = 10;
+	public static int multiplyActual = 0;
 	Logger log = LogManager.getFormatterLogger(dbOpsStepDefAPI.class);
 	LoginPage objLoginPage = new LoginPage(Utilities.getDriver1());
 
-	@Then("^user multiplies \"(.*)\" and \"(.*)\"$")
-	public void userMultipliesTwoNumbers(String num1, String num2) {
+	@Then("^user multiplies \"(.*)\" and \"(.*)\" verify \"(.*)\"$$")
+	public synchronized void userMultipliesTwoNumbers(String num1, String num2, String multiply) {
 //		System.out.println(new Throwable().getStackTrace()[0].getMethodName());
 		num1 = num1.replace("<", "").replace(">", "");
 		num2 = num2.replace("<", "").replace(">", "");
-//		System.out.println("Number 1: "+num1+"; Number 2: "+num2);
-//		System.out.println("Multiplication of numbers Number 1: "+num1+"; Number 2: "+num2+" is: "+Integer.parseInt(num1)*Integer.parseInt(num2));
-		log.info("Multiplication of numbers Number 1: " + num1 + "; Number 2: " + num2 + " is: "
-				+ Integer.parseInt(num1) * Integer.parseInt(num2));
+		int multiplyActual = Integer.parseInt(num1) * Integer.parseInt(num2);
+		int multiplyExp = Integer.valueOf(multiply);
+
+		System.out.println("Expected: "+multiplyExp+", Actual: "+multiplyActual+"; Verify: " + (multiplyExp == multiplyActual));
+
+		log.info("Multiplication of numbers Number 1: " + num1 + "; Number 2: " + num2 + " is: " + multiplyActual);
 	}
 
 	@Then("tests pipe separated params")
--- NewProject/src/test/resources/features/DBFeatures.feature
+++ NewProject/src/test/resources/features/DBFeatures.feature
@@ -2,7 +2,7 @@ Feature: Login feature
     
 	@testParams @DB @tag1 
 	Scenario: Testing params in steps
-		Then user multiplies "33" and "3"
+		Then user multiplies "33" and "3" verify "99"
 		Then tests pipe separated params
 #		|Hello|My|Name|Is|Mr.|Hamid|Raza|
 		|Hello|
@@ -15,20 +15,20 @@ Feature: Login feature
     
 	@testParams @DB @tag1
 	Scenario Outline: Testing params in steps
-		Then user multiplies "12" and "15"
-		Then user multiplies "<num1>" and "<num2>"
+		Then user multiplies "12" and "15" verify "180"
+		Then user multiplies "<num1>" and "<num2>" verify "<multiply>"
 
 		Examples:
-		|num1|num2|
-		|21|17| #357
-		|51|11| #561
-		|12|15| #180
-		|21|21| #441
-		|11|11| #121
-		|4|22|  #88
-		|22|17| #357
-		|52|11| #561
-		|13|15| #180
-		|26|21| #441
-#		|16|11| #121
-#		|45|22|  #88
+		|num1|num2|multiply|
+		|21|17| 357 |
+		|51|11| 561 |
+		|12|15| 180 |
+		|21|21| 441 |
+		|11|11| 121 |
+		|4|22|   88 |
+		|22|17| 374 |
+		|52|11| 572 |
+		|13|15| 195 |
+		|26|21| 546 |
+		|16|11| 176 |
+		|45|22|  990 |
